632893b076bfd3c39328e19c2a209c194e1b00d4
taufanaprialdo99@gmail.com <taufanaprialdo99@gmail.com>
2021-07-13 14:02:28 +0700

    Create Leave Application Project

---
+++ Leave Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Leave_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
