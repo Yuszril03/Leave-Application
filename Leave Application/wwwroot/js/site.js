e142e459bcf5bff677d3accf6809220d093d8c19
taufanaprialdo99@gmail.com <taufanaprialdo99@gmail.com>
2021-07-13 17:02:35 +0700

    Update Landing Page

--- Leave Application/wwwroot/js/site.js
+++ Leave Application/wwwroot/js/site.js
@@ -1,24 +1,12 @@
-﻿////window.addEventListener('load', () => {
-////    let forms = document.getElementsByClassName('needs-validation');
-////    for (let form of forms) {
-////        form.addEventListener('submit', (evt) => {
-////            if (!form.checkValidity()) {
-////                evt.preventDefault();
-////                evt.stopPropagation();
-////            } 
-////            form.classList.add('was-validated');
-////        });
-////    }
-////});
-
-////function Auth() {
-////    Swal.fire({
-////        icon: 'success',
-////        title: 'Success',
-////        text: 'Success to Login.'
-////    }).then((result) => {
-////        if (result.isConfirmed) {
-////            $('#loginModal').modal('hide');
-////        }
-////    });
-////}
+﻿window.addEventListener('load', () => {
+    let forms = document.getElementsByClassName('needs-validation');
+    for (let form of forms) {
+        form.addEventListener('submit', (evt) => {
+            if (!form.checkValidity()) {
+                evt.preventDefault();
+                evt.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        });
+    }
+});
